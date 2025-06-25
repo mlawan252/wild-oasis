b0f1d7e2c29d0763f089bad192c7e02122da2c1f
Muhammad Lawan <mlawan252@gmail.com>
2025-06-25 21:09:07 +0100

    connect supabse to the project

---
+++ src/services/supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = "https://gumnnxfnheeegikgktum.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd1bW5ueGZuaGVlZWdpa2drdHVtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA4NDU5ODEsImV4cCI6MjA2NjQyMTk4MX0.uaqhdX0DnlGCEkUVVR2Bj12cDW3npDjUhRUFKkUvKv8";
+
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
